eb9ca0538b00cb34ae4cd37bca747c71bfaf0a80
Giuliano Damico <giulianodamico2019@gmail.com>
2021-04-24 20:19:45 -0300

    Testimonial and Contact section

--- js/main.js
+++ js/main.js
@@ -183,4 +183,50 @@ function bodyScrollingToggle() {
   }
 })();
 
-/* VIDEO 4 1.30 HS */
+/*------------------------- Testimonial slider ------------------------*/
+(() => {
+  const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth;
+  (prevBtn = document.querySelector(".testi-slider-nav .prev")),
+    (nextBtn = document.querySelector(".testi-slider-nav .next"));
+  activeSlide = sliderContainer.querySelector(".testi-item.active");
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(
+    activeSlide
+  );
+
+  /* Set width of all slides */
+  slides.forEach((slide) => {
+    slide.style.width = slideWidth + "px";
+  });
+  /* Set width of sliderContainer */
+  sliderContainer.style.width = slideWidth * slides.length + "px";
+
+  nextBtn.addEventListener("click", () => {
+    if (slideIndex === slides.length - 1) {
+      slideIndex = 0;
+    } else {
+      slideIndex++;
+    }
+    slider();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (slideIndex === 0) {
+      slideIndex = slides.length - 1;
+    } else {
+      slideIndex--;
+    }
+    slider();
+  });
+  function slider() {
+    /* desactivate existing active slides */
+    sliderContainer
+      .querySelector(".testi-item.active")
+      .classList.remove("active");
+    /* Activate new slide */
+    slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + "px";
+  }
+  slider();
+})();
